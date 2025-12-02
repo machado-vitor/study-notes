4c2130387d8f1cdc95dcf2ebeb7a4ae548617493
Vitor Machado <vitorytre@gmail.com>
2025-12-02 18:55:30 -0300

    monads

--- projects/scala/monads/src/main/scala/Main.scala
+++ projects/scala/monads/src/main/scala/Main.scala
@@ -2,10 +2,10 @@ sealed trait Box[+A]: // sealed so the implementation is on the same file
   def flatMap[B](f: A => Box[B]): Box[B] // the monad operation
   def map[B](f: A => B): Box[B] = flatMap(a => Box(f(a)))  // derived from flatMap
 
-case class Full[A](value: A) extends Box[A]:
+case class Full[A](value: A) extends Box[A]: // class create multiple instances with different data.
   def flatMap[B](f: A => Box[B]): Box[B] = f(value)  // apply function to value
 
-case object Empty extends Box[Nothing]:
+case object Empty extends Box[Nothing]: // Only one instance lives in the entire program/ singleton
   def flatMap[B](f: Nothing => Box[B]): Box[B] = Empty  // short-circuit on empty
 // Nothing is a subtype of everything, Empty can be returned from any Box operation
 object Box:
