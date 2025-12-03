f55d4ee1b70893c4ebbd585b68137725cd6e7d62
Vitor Machado <vitorytre@gmail.com>
2025-12-03 08:43:17 -0300

    by name expression

--- projects/scala/covariants/src/main/scala/Main.scala
+++ projects/scala/covariants/src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 class Lazy[+A](value: => A) { private lazy val internal: A = value }
+// "=> A" is a By-name parameter.
+// It means "Don't evaluate the expression, just remember it for later"
 
 class Animal
 class Dog extends Animal
 
 val lazyDog: Lazy[Dog] = new Lazy(new Dog)
 val lazyAnimal: Lazy[Animal] = lazyDog // Lazy is covariant
+
