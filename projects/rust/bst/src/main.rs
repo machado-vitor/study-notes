c5520ba63f74c76a3ebae4b6a958963cb903cd85
Vitor Machado <vitorytre@gmail.com>
2026-04-02 10:25:17 -0300

    BST

---
+++ projects/rust/bst/src/main.rs
@@ -0,0 +1,86 @@
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    value: T,
+    left: Link<T>,
+    right: Link<T>,
+}
+
+struct Bst<T> {
+    root: Link<T>,
+}
+
+impl<T: Ord> Bst<T> {
+    fn new() -> Self {
+        Bst { root: None }
+    }
+
+    fn insert(&mut self, value: T) {
+        Self::insert_at(&mut self.root, value);
+    }
+
+    fn insert_at(link: &mut Link<T>, value: T) {
+        match link {
+            None => {
+                *link = Some(Box::new(Node {
+                    value,
+                    left: None,
+                    right: None,
+                }));
+            }
+            Some(node) => {
+                if value < node.value {
+                    Self::insert_at(&mut node.left, value);
+                } else if value > node.value {
+                    Self::insert_at(&mut node.right, value);
+                }
+                // equal values are ignored (no duplicates)
+            }
+        }
+    }
+
+    fn contains(&self, value: &T) -> bool {
+        Self::search(&self.root, value)
+    }
+
+    fn search(link: &Link<T>, value: &T) -> bool {
+        match link {
+            None => false,
+            Some(node) => {
+                if *value < node.value {
+                    Self::search(&node.left, value)
+                } else if *value > node.value {
+                    Self::search(&node.right, value)
+                } else {
+                    true
+                }
+            }
+        }
+    }
+
+    fn in_order(&self) -> Vec<&T> {
+        let mut result = Vec::new();
+        Self::in_order_walk(&self.root, &mut result);
+        result
+    }
+
+    fn in_order_walk<'a>(link: &'a Link<T>, result: &mut Vec<&'a T>) {
+        if let Some(node) = link {
+            Self::in_order_walk(&node.left, result);
+            result.push(&node.value);
+            Self::in_order_walk(&node.right, result);
+        }
+    }
+}
+
+fn main() {
+    let mut tree = Bst::new();
+
+    for val in [5, 3, 7, 1, 4, 6, 8, 2] {
+        tree.insert(val);
+    }
+
+    println!("in-order traversal: {:?}", tree.in_order());
+    println!("contains 4? {}", tree.contains(&4));
+    println!("contains 9? {}", tree.contains(&9));
+}
